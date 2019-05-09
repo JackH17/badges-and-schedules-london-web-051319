be231e019b75751329833d5c50228b1d66d7f681
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-09 13:24:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,3 +26,11 @@ def assign_rooms(assignment)
   return array
 end 
 
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+end 
