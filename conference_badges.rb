3fa39a42dd7cd89fd3e751aaf73e87cb8b55b335
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-09 13:22:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+  
+  return "Hello, my name is #{name}."
+  
+end
+
+def batch_badge_creator(names)
+  
+array = []
+
+names.each do |name|
+  array.push("Hello, my name is #{name}.")
+end 
+
+return array 
+end
+
+def assign_rooms(assignment)
+  array = []
+  room = 1
+  
+  assignment.each do |name|
+    array.push("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1 
+  end
+  return array
+end 
+
+def printer
+puts batch_badge_creator
+puts assign_rooms
+
+end
